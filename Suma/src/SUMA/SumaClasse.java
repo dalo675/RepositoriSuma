e7ba6ae4ea1099fe9f16742f49a0ecfa73c689b4
dalo675 <123939374+dalo675@users.noreply.github.com>
2023-05-12 10:27:23 +0200

    Update SumaClasse.java

--- Suma/src/SUMA/SumaClasse.java
+++ Suma/src/SUMA/SumaClasse.java
@@ -7,5 +7,8 @@ package SUMA;
  */
 public class SumaClasse {
 
-
+    // Mètode sumar, retorna el valor de la suma de num1 i num2
+    public int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
 }
