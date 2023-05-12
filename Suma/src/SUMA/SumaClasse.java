cee52072d7b99f7002d9d46c65ae2ba11d5f8772
dalo675 <123939374+dalo675@users.noreply.github.com>
2023-05-12 10:30:13 +0200

    Update SumaClasse.java

--- Suma/src/SUMA/SumaClasse.java
+++ Suma/src/SUMA/SumaClasse.java
@@ -7,5 +7,9 @@ package SUMA;
  */
 public class SumaClasse {
 
+// Metode valor absolut, retorna la suma dels valors absoluts de num1 i num2
+    public int sumaValorAbsolut(int num1, int num2) {
+        return Math.abs(num1) + Math.abs(num2);
+    }
 
 }
